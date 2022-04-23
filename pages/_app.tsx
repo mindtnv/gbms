df414e160eadbfe29897b295ee148f028686f548
midntnv <mindtnv@gmail.com>
2022-04-23 18:04:41 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import AppLayout from "../components/layouts/AppLayout";
+import Fonts from "../components/Fonts";
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
     </ChakraProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,42 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Box, Heading, VStack, Text, StackDivider } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <Container centerContent>
-      <Heading as="h1">GBMS Project</Heading>
-    </Container>
+    <VStack spacing={12} divider={<StackDivider></StackDivider>}>
+      <Box>
+        <Heading as="h2">gbms aka godblessmysoul</Heading>
+        <Text>Fullstack JS/TS/.NET developer | Design stealer</Text>
+      </Box>
+      <Box>
+        <Heading as="h2">Тесты</Heading>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          eveniet ipsa quia autem tempore numquam cum velit harum? Temporibus
+          quaerat ipsa aliquid maiores culpa deleniti officiis sapiente iusto
+          veritatis molestias?
+        </Text>
+      </Box>
+      <Box>
+        <Heading as="h2">О себе</Heading>
+        <Text>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+          culpa, fuga laboriosam quae alias expedita aperiam cupiditate adipisci
+          nesciunt eligendi. Aliquam illum autem maxime cupiditate nihil
+          quaerat, similique sint culpa.
+        </Text>
+      </Box>
+
+      <Box>
+        <Heading as="h2">Мои проекты</Heading>
+        <Text>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          dolor nisi commodi accusantium aspernatur earum qui quis nihil dolorum
+          molestias! Nulla assumenda earum fugiat molestiae magnam illo itaque
+          laboriosam tenetur?
+        </Text>
+      </Box>
+    </VStack>
   );
 };
 
