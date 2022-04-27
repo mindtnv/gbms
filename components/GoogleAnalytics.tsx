5521903b8c78739f23ecb48f21c394524359c6cc
mindtnv <mindtnv@gmail.com>
2022-04-27 19:55:28 +0300

    Add GoogleAnalytics

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,28 @@
+import Script from "next/script";
+
+const GoogleAnalytics = () => {
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-PC0ZTQHQ0S"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PC0ZTQHQ0S', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+};
+
+// @ts-ignore
+export default GoogleAnalytics;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Head, Html, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        <GoogleAnalytics />
+      </Head>
       <body style={{ overflowY: "scroll" }}>
         <ColorModeScript />
         <Main />
